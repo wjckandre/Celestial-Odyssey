6a0a10d64b8b4a4190d215c3d3ae8e52c991cd94
wjckandre <wojcik.andre2.0@gmail.com>
2023-09-27 20:20:44 -0400

    premier plateformer maison (base)

--- main.py
+++ main.py
@@ -1,42 +1,84 @@
 import pygame
+import sys
 
-# pygame setup
+# Initialisation de Pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+# Paramètres de l'écran
+largeur, hauteur = 800, 600
+ecran = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Jeu de Plateforme")
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+# Couleurs
+blanc = (255, 255, 255)
+bleu = (0, 0, 255)
+
+# Personnage
+personnage = pygame.Rect(50, 500, 40, 40)
+personnage_vitesse = 1
+saut = False
+saut_compteur = 400
+
+
+# Plateformes
+plateformes = [pygame.Rect(100, 300, 200, 10),
+              pygame.Rect(400, 450, 200, 10),
+              pygame.Rect(200, 200, 200, 10)]
+
+# Boucle de jeu
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    touches = pygame.key.get_pressed()
+    if touches[pygame.K_LEFT]:
+        personnage.x -= personnage_vitesse
+    if touches[pygame.K_RIGHT]:
+        personnage.x += personnage_vitesse
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    if not saut:
+        if touches[pygame.K_SPACE]:
+            saut = True
+            
+    else:
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_DOWN]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_LEFT]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_RIGHT]:
-        player_pos.x += 300 * dt
+        if saut_compteur > 200:
+            personnage.y -= 1
+            saut_compteur -= 1
+        elif saut_compteur > 0:
+            personnage.y += 1
+            saut_compteur -= 1
+        else:
+            saut = False
+            saut_compteur = 400
+        
 
-    # flip() the display to put your work on screen
+
+    # Gravité
+    if personnage.y < hauteur - 50 and not saut:
+        personnage.y += 1
+
+    # Collision avec les plateformes
+    for plateforme in plateformes:
+        if personnage.colliderect(plateforme):
+            saut = False
+            saut_compteur = 400
+            personnage.y = plateforme.y - personnage.height
+
+    # Effacer l'écran
+    ecran.fill(blanc)
+
+    # Dessiner le personnage
+    pygame.draw.rect(ecran, bleu, personnage)
+
+    # Dessiner les plateformes
+    for plateforme in plateformes:
+        pygame.draw.rect(ecran, bleu, plateforme)
+
+    # Mettre à jour l'affichage
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
+# Quitter Pygame
 pygame.quit()
